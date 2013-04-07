97739ec85b80784b0c86389182ee08eb63a8c08e
Christoph Herrmann <developer@dragonprojects.de>
2013-04-07 05:35:11 +0200

    "global.php" als Vorlage zur Anpassung der eigenen Einstellungsdatei

---
+++ config/global.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2013 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServer
+ */
+
+/**
+ * @return array
+ */
+/*
+ * re
+ */
+return [
+    'application' => [
+    	'name' => '%name%',
+    	'version' => '%version%',
+    	'copyright' => '%copyright%',
+    ],
+    'apicachefile' => '%filepath%',
+    'apiclasses' => [
+		'%classname%' => '%namespace%', 
+		'%classname%' => '%namespace%', 
+    ],
+];
